f39527bfb45c8e820caac420ea2ba6a9560eaf9b
karem <karem2003.kk@gmail.com>
2024-01-30 15:33:55 +0200

    2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'School_AI',
+      title: 'School_AI1',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: bgColor,
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
